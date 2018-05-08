1783e75aee416af2cc85293e5e63d660b4af0e3f
dravit <dravit.info@gmail.com>
2018-05-08 16:04:21 +0530

    Add files via upload

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -4,5 +4,13 @@ public class Constants {
 
     public static final String EMPTY_STRING = "";
 
-    public static final String DELIMETER = ",";
+    public static final String DELIMITER = ",";
+
+    public static final String KEY_SEPARATOR = "_";
+
+    public static final String CITY_COUNTRY = "City / Country";
+
+    public static final String GENDER = "Gender";
+
+    public static final String PERCAPITAINCOME = "PerCapitaIncome";
 }
---
+++ src/utils/CurrencyEnums.java
@@ -0,0 +1,6 @@
+package utils;
+
+public enum CurrencyEnums {
+
+    INR, USD, HKD, SGP, GBP;
+}
