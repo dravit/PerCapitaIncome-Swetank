682b4930f37051165a3e55d4ff580c11263cb760
dravit <dravit.info@gmail.com>
2018-05-08 16:01:56 +0530

    Add files via upload

--- src/converters/CurrencyConverter.java
+++ src/converters/CurrencyConverter.java
@@ -1,7 +1,27 @@
 package converters;
 
-/**
- * Created by dravi on 07-05-2018.
- */
+import dataObjects.AverageIncomeBean;
+
+import java.util.List;
+
 public class CurrencyConverter {
+
+    public static void covertIncomeToUSD(AverageIncomeBean averageIncomeBean) {
+        switch (averageIncomeBean.getCurrency()) {
+            case INR:
+                averageIncomeBean.setAvgIncome(averageIncomeBean.getAvgIncome() / 67.25);
+                break;
+            case USD:
+                break;
+            case HKD:
+                averageIncomeBean.setAvgIncome(averageIncomeBean.getAvgIncome() / 7.85);
+                break;
+            case SGP:
+                averageIncomeBean.setAvgIncome(averageIncomeBean.getAvgIncome() / 1.34);
+                break;
+            case GBP:
+                averageIncomeBean.setAvgIncome(averageIncomeBean.getAvgIncome() / 0.84);
+                break;
+        }
+    }
 }
