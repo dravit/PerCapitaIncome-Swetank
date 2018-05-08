923983d51dffb13d149868c0b32fe204c8281f26
dravit <dravit.info@gmail.com>
2018-05-08 16:08:38 +0530

    Create ReadFromCSVTest.java

---
+++ tests/dataReader/ReadFromCSVTest.java
@@ -0,0 +1,27 @@
+package dataReader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ReadFromCSVTest {
+
+    private static final String INPUT_FILENAME = "resources\\input\\AverageIncomeIncomeInput.csv";
+
+    DataReader dataReader;
+
+    @Before
+    public void setUp() throws Exception {
+        dataReader = new ReadFromCSV(INPUT_FILENAME);
+    }
+
+    @Test
+    public void readLinesFromFile() throws IOException {
+        List<String> linesFromFile = dataReader.readLinesFromFile();
+        assertEquals(21, linesFromFile.size());
+    }
+}
